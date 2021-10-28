fa58e84b3e0efc6a72652915e8c84f1acf90af93
Selien Xavier Yorbandi <74626047+selienyorbandi@users.noreply.github.com>
2021-10-28 16:15:52 -0300

    Validacion visual con bootstrap de formularios

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 /* _______Menu hamburguesa________*/
-const hamMenu = document.querySelector('.hamMenu');
-const navLinks = document.querySelector('.navLinks');
+const hamMenu = document.querySelector(".hamMenu");
+const navLinks = document.querySelector(".navLinks");
 
-hamMenu.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+hamMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
 });
+
+/* _______Validación de formularios________ (solo feedback de bootstrap por ahora)*/
+
+const formularios = document.querySelector('.formularios');
+const formSubmit = document.querySelector('.submitBtn');
+
+formSubmit.addEventListener("click", (e) => {
+  formularios.classList.add("was-validated");
+});
+
