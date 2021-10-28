334f24319001f6849a5ff841534a469c4075a252
Selien Xavier Yorbandi <74626047+selienyorbandi@users.noreply.github.com>
2021-10-28 18:17:22 -0300

    Animacion X al abrir menu hamburguesa

--- js/main.js
+++ js/main.js
@@ -1,9 +1,11 @@
 /* _______Menu hamburguesa________*/
 const hamMenu = document.querySelector(".hamMenu");
 const navLinks = document.querySelector(".navLinks");
+const hamMenuBars = document.querySelectorAll('.hamMenu span');
 
 hamMenu.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+  hamMenuBars.forEach(child => {child.classList.toggle('active')})
 });
 
 /* _______Validación de formularios________ (solo feedback de bootstrap por ahora)*/
