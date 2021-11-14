79ef9a14d9d42ad194e569e5d91388db0e79382c
Selien Xavier Yorbandi <74626047+selienyorbandi@users.noreply.github.com>
2021-11-13 23:39:45 -0300

    Alerta tras envio de formulario de contacto exitoso; Unificacion del script en el archivo con el nombre pedido por el profesor

--- Frontend/js/funcionesGrupo4.js
+++ Frontend/js/funcionesGrupo4.js
@@ -1,16 +1,32 @@
-  
-  /* Inicio de sesión */
+/* _______Menu hamburguesa________*/
+const hamMenu = document.querySelector(".hamMenu");
+const navLinks = document.querySelector(".navLinks");
+const hamMenuBars = document.querySelectorAll('.hamMenu span');
+
+hamMenu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+  hamMenuBars.forEach(child => {child.classList.toggle('active')})
+});
+
+/* _______Validación de formularios________ Punto 1 IEFI Programación Web
+(solo feedback de bootstrap por ahora)*/
+
+const formularios = document.querySelector('.formularios');
+const formSubmit = document.querySelector('.submitBtn');
+
+formSubmit.addEventListener("click", (e) => {
+  formularios.classList.add("was-validated");
+});  
+
+/* Procesado de datos enviados en los inputs del formulario de registro -- Punto 4 */
   
   function modalRegistro() {
     const formRegistro = document.getElementById("formRegistro");
-    const regSubmit = document.getElementById("regSubmit");
     var regisModal = new bootstrap.Modal(document.getElementById('modalForm'), {
       keyboard: false
     });
     const regModalBody = document.querySelector("#modalForm .modal-body");
     const regisConfirm = document.getElementById("regisConfirm");
-  
-  
   
     let nombreUsuario = document.getElementById("nombreRegis");
     let apellidoUsuario = document.getElementById("apellidoRegis");
@@ -19,8 +35,6 @@
     let paisUsuario = document.getElementById("paisRegis");
     let provinciaUsuario = document.getElementById("provinciaRegis");
     let codPosUsuario = document.getElementById("cpRegis");
-  
-    
   
     formRegistro.addEventListener("submit", (e) => {
       e.preventDefault();
@@ -38,8 +52,30 @@
       regisModal.show();
     });
   
-    regisConfirm.addEventListener("click", ()=> {window.location.href = "../Frontend/login.html"});
-    
+    regisConfirm.addEventListener("click", ()=> {window.location.pathname = "/login.html"}); 
+    //Para probarlo en local hay que cambiar el pathname por "../Frontend/login.html"
+        
   }
-  
-  modalRegistro();
+  if (window.location.pathname === "/registro.html" || window.location.pathname === "/a5-g4-tienda-on-line/Frontend/registro.html") {modalRegistro()}; 
+
+
+/* Alerta tras envio de formulario de contacto -- Punto 3 IEFI Programacion Web 1 */
+function alertaContacto () {
+  const formContacto = document.querySelector("#formContacto");
+  formContacto.addEventListener("submit", (e)=> {
+    e.preventDefault();
+    Swal.fire({
+      icon: 'success',
+      title: "Mensaje enviado correctamente",
+      text: 'Responderemos su consulta a la brevedad',
+      showConfirmButton: false,
+    })
+    setInterval(() => {
+      window.location.pathname = "/index.html" // Para probar en local "/a5-g4-tienda-on-line/Frontend/index.html"
+    }, 2500);
+  })
+}
+
+if (window.location.pathname === "/contacto.html" || window.location.pathname === "/a5-g4-tienda-on-line/Frontend/contacto.html") {alertaContacto()}; 
+
+
